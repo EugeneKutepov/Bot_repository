bba42d498a35048190a09a1011389d397e194740
Evgeny Kutepov <kutepowv@gmail.com>
2023-02-16 13:47:18 +0000

    Commit with send_copy

--- read_web.py
+++ read_web.py
@@ -3,14 +3,16 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "https://www.vsegei.ru/ru/public/sprav/geodictionary/"
-tables = pd.read_html(url)
+#url = 'https://www.kinopoisk.ru/lists/top250/'
 
-print(tables)
+# tables = pd.read_html(url)
+#
+# print(tables)
 
-# # делаем запрос и получаем html
-# html_text = requests.get(url).text
-#
-# # используем парсер lxml
-# soup = BeautifulSoup(html_text, 'lxml')
-#
-# print(soup)
+# делаем запрос и получаем html
+html_text = requests.get(url).text
+
+# используем парсер lxml
+soup = BeautifulSoup(html_text, 'lxml')
+
+print(soup)
