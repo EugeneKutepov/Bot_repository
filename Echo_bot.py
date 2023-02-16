bc920d87a1e431a3475c8454103769700b36dd7c
Evgeny Kutepov <kutepowv@gmail.com>
2023-02-16 09:26:12 +0000

    Photo and voice added

--- Echo_bot.py
+++ Echo_bot.py
@@ -1,6 +1,8 @@
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
+from aiogram.types import ContentType
+from aiogram import F
 
 
 API_TOKEN: str = '6054779759:AAFoSpHEE13tJijeO1rsEtPoHzl9NhV0p50'
@@ -21,6 +23,17 @@ async def process_help_command(message: Message):
                          'я пришлю тебе твое сообщение')
 
 
+# Этот хэндлер будет срабатывать на отправку боту фото
+async def send_photo_echo(message: Message):
+    print(message)
+    await message.reply_photo(message.photo[0].file_id)
+
+
+async def send_voice_echo(message: Message):
+    print(message)
+    await message.reply_voice(message.voice.file_id)
+
+
 # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
 # кроме команд "/start" и "/help"
 async def send_echo(message: Message):
@@ -30,6 +43,8 @@ async def send_echo(message: Message):
 # Регистрируем хэндлеры
 dp.message.register(process_start_command, Command(commands=["start"]))
 dp.message.register(process_help_command, Command(commands=['help']))
+dp.message.register(send_photo_echo, F.content_type == ContentType.PHOTO)
+dp.message.register(send_voice_echo, F.voice)
 dp.message.register(send_echo)
 
 if __name__ == '__main__':
