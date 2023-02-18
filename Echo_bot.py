7a7af8e934d7c2abc86626107a5253fb0e9409f2
Evgeny Kutepov <kutepowv@gmail.com>
2023-02-18 17:53:45 +0000

    Find numbers

--- Echo_bot.py
+++ Echo_bot.py
@@ -1,5 +1,7 @@
+from typing import Text
+
 from aiogram import Bot, Dispatcher
-from aiogram.filters import Command
+from aiogram.filters import Command, BaseFilter
 from aiogram.types import Message
 from aiogram.types import ContentType
 from aiogram import F
@@ -17,26 +19,46 @@ async def process_start_command(message: Message):
     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
 
-# Этот хэндлер будет срабатывать на команду "/help"
-async def process_help_command(message: Message):
-    await message.answer('Напиши мне что-нибудь и в ответ '
-                         'я пришлю тебе твое сообщение')
+# Этот фильтр будет проверять наличие неотрицательных чисел
+# в сообщении от пользователя, и передавать в хэндлер их список
+class NumbersInMessage(BaseFilter):
+    async def __call__(self, message: Message) -> bool | dict[str, list[int]]:
+        numbers = []
+        # Разрезаем сообщение по пробелам, нормализуем каждую часть, удаляя
+        # лишние знаки препинания и невидимые символы, проверяем на то, что
+        # в таких словах только цифры, приводим к целым числам
+        # и добавляем их в список
+        for word in message.text.split():
+            normalized_word = word.replace('.', '').replace(',', '').strip()
+            if normalized_word.isdigit():
+                numbers.append(int(normalized_word))
+        # Если в списке есть числа - возвращаем список по ключу 'numbers'
+        if numbers:
+            return {'numbers': numbers}
+        return False
 
 
-# Этот хэндлер будет срабатывать на любые ваши сообщения,
-# кроме команд "/start" и "/help"
-@dp.message()
-async def send_echo(message: Message):
-    try:
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        await message.reply(text='Данный тип апдейтов не поддерживается '
-                                 'методом send_copy')
+# Этот хэндлер будет срабатывать, если сообщение пользователя
+# начинается с фразы "найди числа" и в нем есть числа
+@dp.message(Text(startswith='найди числа', ignore_case=True),
+            NumbersInMessage())
+# Помимо объекта типа Message, принимаем в хэндлер список чисел из фильтра
+async def process_if_numbers(message: Message, numbers: list[int]):
+    await message.answer(
+            text=f'Нашел: {str(", ".join(str(num) for num in numbers))}')
+
+
+# Этот хэндлер будет срабатывать, если сообщение пользователя
+# начинается с фразы "найди числа", но в нем нет чисел
+@dp.message(Text(startswith='найди числа', ignore_case=True))
+async def process_if_not_numbers(message: Message):
+    await message.answer(
+            text='Не нашел что-то :(')
 
 
 # Регистрируем хэндлеры
 dp.message.register(process_start_command, Command(commands=["start"]))
-dp.message.register(process_help_command, Command(commands=['help']))
+#dp.message.register(process_help_command, Command(commands=['help']))
 
 if __name__ == '__main__':
     dp.run_polling(bot)
