ee9fbea5ed9bba303f644b6d9fc3a4197ae3cd04
Evgeny Kutepov <kutepowv@gmail.com>
2023-02-27 19:06:56 +0000

    Iterating images

--- parsing/read_web.py
+++ parsing/read_web.py
@@ -1,24 +1,9 @@
 import pandas as pd
 import requests
-from random import choice
-from bs4 import BeautifulSoup
 
-#url = "https://www.vsegei.ru/ru/public/sprav/geodictionary/"
-#url = 'https://www.kinopoisk.ru/lists/top250/'
 
-# tables = pd.read_html(url)
-#
-# print(tables)
-
-# делаем запрос и получаем html
-#html_text = requests.get(url).text
-
-# используем парсер lxml
-#soup = BeautifulSoup(html_text, 'lxml')
-
-url = 'http://httpbin.org/ip'
-
-response = requests.get(url=url, stream=True)
-with open('file.mp4', 'wb') as video:
-    for piece in response.iter_content(chunk_size=100000):
-        video.write(piece)
+#url = 'http://httpbin.org/ip'
+for i in range(1, 161):
+    response = requests.get(url=f'https://parsinger.ru/img_download/img/ready/{i}.png')
+    with open(f'img/image{i}.jpeg', 'wb') as file:
+        file.write(response.content)
