beebd3e3199aaddd0b71c90fc49b681692eacb38
Evgeny Kutepov <kutepowv@gmail.com>
2023-02-27 13:35:53 +0000

    Keyboard builder

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 from environs import Env
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart, Text
-from aiogram.types import (KeyboardButton, Message, ReplyKeyboardMarkup,
-                           ReplyKeyboardRemove, BotCommand, InlineKeyboardButton, InlineKeyboardMarkup)
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, KeyboardButtonPollType
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.types import (Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery)
 
 env = Env()  # Создаем экземпляр класса Env
 env.read_env()  # Методом read_env() читаем файл .env и загружаем из него переменные в окружение
@@ -12,25 +10,63 @@ env.read_env()  # Методом read_env() читаем файл .env и заг
 bot:Bot = Bot(env('BOT_TOKEN'))
 dp: Dispatcher = Dispatcher()
 
-# Создаем объекты инлайн-кнопок
-url_button_1: InlineKeyboardButton = InlineKeyboardButton(
-    text='Курс "Телеграм-боты на Python и AIOgram"',
-    url='https://stepik.org/120924')
-url_button_2: InlineKeyboardButton = InlineKeyboardButton(
-    text='Документация Telegram Bot API',
-    url='https://core.telegram.org/bots/api')
+LEXICON: dict[str, str] = {
+    'but_1': 'Кнопка 1',
+    'but_2': 'Кнопка 2',
+    'but_3': 'Кнопка 3',
+    'but_4': 'Кнопка 4',
+    'but_5': 'Кнопка 5',
+    'but_6': 'Кнопка 6',
+    'but_7': 'Кнопка 7',}
 
-# Создаем объект инлайн-клавиатуры
-keyboard: InlineKeyboardMarkup = InlineKeyboardMarkup(
-    inline_keyboard=[[url_button_1],
-                     [url_button_2]])
+BUTTONS: dict[str, str] = {
+    'btn_1': '1',
+    'btn_2': '2',
+    'btn_3': '3',
+    'btn_4': '4',
+    'btn_5': '5',
+    'btn_6': '6',
+    'btn_7': '7',
+    'btn_8': '8',
+    'btn_9': '9',
+    'btn_10': '10',
+    'btn_11': '11'}
+
+
+# Функция для генерации инлайн-клавиатур "на лету"
+def create_inline_kb(width: int,
+                     *args: str,
+                     **kwargs: str) -> InlineKeyboardMarkup:
+    # Инициализируем билдер
+    kb_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
+    # Инициализируем список для кнопок
+    buttons: list[InlineKeyboardButton] = []
+
+    # Заполняем список кнопками из аргументов args и kwargs
+    if args:
+        for button in args:
+            buttons.append(InlineKeyboardButton(
+                text=LEXICON[button] if button in LEXICON else button,
+                callback_data=button))
+    if kwargs:
+        for button, text in kwargs.items():
+            buttons.append(InlineKeyboardButton(
+                text=text,
+                callback_data=button))
+
+    # Распаковываем список с кнопками в билдер методом row c параметром width
+    kb_builder.row(*buttons, width=width)
+
+    # Возвращаем объект инлайн-клавиатуры
+    return kb_builder.as_markup()
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
-# и отправлять в чат клавиатуру c url-кнопками
+# и отправлять в чат клавиатуру
 @dp.message(CommandStart())
 async def process_start_command(message: Message):
-    await message.answer(text='Это инлайн-кнопки с параметром "url"',
+    keyboard = create_inline_kb(2, 'but_1', 'but_3', 'but_7')
+    await message.answer(text='Вот такая получается клавиатура',
                          reply_markup=keyboard)
 
 
